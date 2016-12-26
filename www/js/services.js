90b4a6cff826259bef2c819ced59617835ecdd88
Shubham Jaiswal <shubham.jaiswal357@gmail.com>
2016-12-27 00:21:55 +0530

    Little Entries

---
+++ www/js/services.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Shubham on 20-12-2016.
+ */
+angular.module('starter.services', [])
+
+  .factory("events",function ($firebaseArray) {
+    var eventsRef = firebase.database().ref().child('events');
+    return $firebaseArray(eventsRef);
+  });
